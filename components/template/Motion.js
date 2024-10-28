5e6f9842dfb5426f755377ebe43e9b815c1fc98f
yunkhngn <rogkhoa.mail@gmail.com>
2024-10-29 00:32:29 +0700

    update animation;

--- components/template/Motion.js
+++ components/template/Motion.js
@@ -3,20 +3,17 @@ import { motion} from 'framer-motion'
 const animation = {
     initial: {
       opacity: 0,
-      y : -50,
     },
     animate: {
       opacity: 1,
-      y : 0,
     },
     exit: {
       opacity: 0,
-      y : -50,
     }
   }
 
   const transition = {
-    duration: 0.4,
+    duration: 0.2,
     ease: [0.25, 0.1, 0.25, 1], // cubic-bezier for smooth transitions
     type: "easeInOut",
 }
