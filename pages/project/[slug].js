bfd2093c126e69869dccd37e37fc9292cd71b014
yunkhngn <rogkhoa.mail@gmail.com>
2024-10-30 14:23:45 +0700

    Remore ref

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -146,9 +146,6 @@ const ProjectInfo = ({data}) => {
       </Div>
       }
     </div>
-    <Text textSize="subheader" w={{xs: "90%", md:"60%"}} m={{ t: "2em" }}>
-        <strong>References:</strong> {data.references}
-      </Text>
       <hr className="seperate"/>
     </Div>
         <Link href="/project">
