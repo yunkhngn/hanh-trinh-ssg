e675ad4e432f89c81affaca3083c81397ab1df4b
yunkhngn <rogkhoa.mail@gmail.com>
2024-11-01 15:48:14 +0700

    Remove ref

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -41,7 +41,6 @@ export async function getStaticPaths() {
             link: item.fields.link,
             year: item.fields.year,
             group: item.fields.group,
-            references: item.fields.references,
         };
   
         return {
